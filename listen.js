98f59a8080660daf95f969f2587fe3dfc8f2b8ce
Jack Marten (Discord Dev) <31144127+malekdrmalek@users.noreply.github.com>
2017-11-18 12:59:59 +0300

    Update listen.js

--- listen.js
+++ listen.js
@@ -1,6 +1,15 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+client.on('ready',  () => {
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+  console.log('~                ~  By : #coloring Team ~                     ~');
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+});
+
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag} !`);
         LOka.user.setActivity("Your Mom Moaing",{type: 'LISTENING'});
